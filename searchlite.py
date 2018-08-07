eb95bc6f352b2993fa1a5628cab4148547b4cd63
Skyler Lehmkuhl <skyler_lehmkuhl@cable.comcast.com>
2018-08-07 15:00:36 -0400

    Initial functionality

--- searchlite.py
+++ searchlite.py
@@ -1,10 +1,60 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def search(string, field, node):
-	if string[0] in node:
-		return search(string[1:], field, node)
-	elif node['_IS_LEAF']:
-		return [i for i in node if string in i]
-	else:
-		return False
+"""Mini searching db."""
+
+import pprint
+import uuid
+
+MAX_BUCKET_SIZE = 20
+IDS = {}
+
+
+class DB(object):
+    """Mini elasticsearch-like database."""
+
+    def __init__(self):
+        """Create new empty db object."""
+        self.prefixes = {}
+
+    def add(self, item):
+        """Item is an arbitrary dictionary to be added to the database.
+
+        Keys are indexed
+        """
+        itemid = uuid.uuid4()
+        IDS[itemid] = item
+        for key in item.keys():
+            prefixes = self.prefixes
+            for i, char in enumerate(key):
+                if char in prefixes:
+                    if i == len(key) - 1:
+                        if item[key] in prefixes[char][0]:
+                            prefixes[char][0][item[key]].append(itemid)
+                        else:
+                            prefixes[char][0][item[key]] = [itemid]
+                    else:
+                        prefixes = prefixes[char][1]
+                    # if len(prefixes[char]) > MAX_BUCKET_SIZE:
+
+                else:
+                    if i == len(key) - 1:
+                        prefixes[char] = [{item[key]:[itemid]}]
+                    else:
+                        prefixes[char] = [{}, {}]
+                        prefixes = prefixes[char][1]
+
+    def get(self, field, val):
+        """Get an item stored in the db.
+
+        Raises KeyError if no such item exists.
+        """
+        prefixes = self.prefixes
+        for char in field[:-1]:
+            prefixes = prefixes[char][1]
+        return [IDS[i] for i in prefixes[field[-1]][0][val]]
+
+    def pprint(self):
+        """Pretty print internals of object."""
+        pprint.pprint(IDS)
+        pprint.pprint(self.prefixes)
